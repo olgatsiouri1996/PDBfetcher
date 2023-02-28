7bce0cba126014d43addd1ded71d6e09fb3d38d1
Olga Tsiouri <olgatsiouri@outlook.com>
2023-02-28 23:46:06 +0200

    .

---
+++ README.md
@@ -0,0 +1,32 @@
+# PDBfetcher
+A cross platform GUI app for windows and linux to download pdb or fasta files from Protein Data Bank by using pdb identifiers
+
+![](img/program_gui.png)
+
+### The GUI apps and input/output examples can be downloaded here: [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.7686299.svg)](https://doi.org/10.5281/zenodo.7686299)
+
+## Windows Dependences
+Windows 10 version 1803 or later
+To find the windows version of your machine go to the search bar and type:
+
+![](img/find_windows_version.png)
+
+then click on the winver program to see which windows 10 version do you have  
+
+## linux Dependences
+Install the curl tool by opening the terminal and pasting the following: 
+
+```shell
+sudo apt update
+sudo apt install curl
+```  
+the input can be only 1 pdb id or a list of pdb ids as seen here:
+
+![](img/input_example.png) 
+
+while the output data can look like this:
+
+![](img/output_example.png) 
+
+
+
---
+++ src/linux/PDBfetcher.py
@@ -0,0 +1,39 @@
+# python3
+from gooey import *
+import os
+# input parameters
+@Gooey(required_cols=0, program_name='PDBfetcher', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
+def main():
+    ap = GooeyParser(description="Fetch pdb or fasta file/s from PDB")
+    ap.add_argument("-type", "--output type", required=False, type=str, widget='Dropdown', default='1 pdb file', choices=['1 pdb file','1 fasta file','many pdb files','many fasta files','1 multi-fasta file'], help="output type to choose")
+    ap.add_argument("-in", "--input txt", required=False, widget='FileChooser', help="1-column input txt file with PDB ids")
+    ap.add_argument("-pdb", "--pdb id", required=False, type=str, help="PDB id to retrieve the sequence or pdb file")
+    ap.add_argument("-dir", "--output directory", required=False, type=str,widget='DirChooser', help="output directory to save the pdb or fasta file/s per pdb id")
+    ap.add_argument("-mfa", "--multi fasta", required=False,widget='FileSaver', help="output multi-fasta file to save sequences for all input pdb ids")
+    args = vars(ap.parse_args())
+    # choose output  
+    outy = args['output type']
+    match outy:
+        case '1 pdb file':
+            os.chdir(args['output directory'])
+            os.system(''.join(['curl -s https://files.rcsb.org/view/',args['pdb id'],'.pdb',' > ',args['pdb id'],'.pdb']))
+        case '1 fasta file':
+            os.chdir(args['output directory'])
+            os.system(''.join(['curl -s https://www.rcsb.org/fasta/entry/',args['pdb id'],'/display',' > ',args['pdb id'],'.fasta']))
+        case 'many pdb files':
+            ids = (str(line.rstrip()) for line in open(args['input txt']))
+            os.chdir(args['output directory'])
+            for i in ids:
+                os.system(''.join(['curl -s https://files.rcsb.org/view/',i,'.pdb',' > ',i,'.pdb']))
+        case 'many fasta files':
+            ids = (str(line.rstrip()) for line in open(args['input txt']))
+            os.chdir(args['output directory'])
+            for i in ids:
+                os.system(''.join(['curl -s https://www.rcsb.org/fasta/entry/',i,'/display',' > ',i,'.fasta']))
+        case '1 multi-fasta file':
+            ids = (str(line.rstrip()) for line in open(args['input txt']))
+            for i in ids:
+                os.system(''.join(['curl -s https://www.rcsb.org/fasta/entry/',i,'/display',' >> ',args['multi fasta']]))        
+
+if __name__ == '__main__':
+    main()
